e9bd7386f17830aee3ddcc4be1577d9d976099c8
Alon <alon212@gmail.com>
2020-12-15 12:48:06 +0200

    build.sh script working!

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ CC=gcc
 
 PROGRAMS="A B"
 
-
 A_SRCS="point.c point_main.c"
 A_HDRS="point.h"
 A_EXEC=point
@@ -13,71 +12,72 @@ B_SRCS="person.c person_main.c"
 B_HDRS="person.h"
 B_EXEC=person
 
-for x in $PROGRAMS
-	do
+function build_obj() #//A_SRCS="point.c point_main.c"
+{
+    ISRCS=$@
+    unset OBJS
+    for SRC in $ISRCS
+    do
+        OBJ=${SRC/%.c/.o}
 
-	SRCS="${x}_SRCS"
+        OBJS="$OBJ $OBJS"
+
+        for HDR in ${!HDRS} #//A_HDRS="point.h"
+        do
+
+            if [ $HDR -nt $OBJ ] || [ $SRC -nt $OBJ ]
+            then
+                echo "Compilling $SRC..."
+                $CC -c -o $OBJ $SRC
+
+
+            fi
+
+
+        done
+    done
+}
+function build_exec()
+{
+    tput setaf 1; echo "----->" $@
+    tput sgr0;
+    IOBJS=$@
+
+    for OBJ in $IOBJS
+    do
+        #echo $OBJ
+        if [ ${OBJ} -nt "./${!EXEC}" ]
+        then
+            echo "Building ${!EXEC}..."
+            $CC -o ${!EXEC} $IOBJS
+            break
+        fi
+
+    done
+}
+
+for x in $PROGRAMS
+do
+
+    SRCS="${x}_SRCS"
     HDRS="${x}_HDRS"
     EXEC="${x}_EXEC"
-	
-
-	if [ $1 ] && [ $1 == "clean" ]
-	then
-		echo "Deleting obj and exec..."
-			rm -f *.o ${!EXEC}
-			echo "DONE"
-			exit
-	fi
 
 
 
-	function build_obj()
-	{
-		ISRCS=$@
+    if [ $1 ] && [ $1 == "clean" ]
+    then
+        echo "Deleting obj and exec..."
+        rm -f *.o ${!EXEC}
+        echo "DONE"
+        exit
+    fi
 
-		
 
-		for SRC in $ISRCS
-		do
-			OBJ=${SRC/%.c/.o}
-		
-			OBJS="$OBJ $OBJS"
-			
-			for HDR in ${!HDRS}
-			do
-			
-				if [ $HDR -nt $OBJ ] || [ $SRC -nt $OBJ ]
-				then
-					echo "Compilling $SRC..."
-					$CC -c -o $OBJ $SRC
-					
-				fi
-			
-			done
+    build_obj ${!SRCS}
+    build_exec $OBJS
 
-		done
 
-	}
 
-	function build_exec()
-	{
-		IOBJS=$@
-		#echo $IOBJS
-		for OBJ in $IOBJS
-		do
-			if [ $OBJ -nt ${!EXEC} ]
-			then
-				echo "Building ${!EXEC}..."
-				$CC -o ${!EXEC} $IOBJS
-				break
-			fi
-		done
-	}
 
-   
-	build_obj ${!SRCS}
-	build_exec $OBJS
-	 
 done
-
-
